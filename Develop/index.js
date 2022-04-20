baeed048ac636795c3c428f1a8ff24a4067dfa59
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-04-20 15:10:44 -0500

    inport markdown gen. created write file function

--- Develop/index.js
+++ Develop/index.js
@@ -2,8 +2,10 @@
 // https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
 
 // TODO: Include packages needed for this application
+
 // Input
 const inquirer = require('inquirer');
+const genMarkdown = require('./utils/generateMarkdown.js')
 // Export
 const fs = require('fs');
 
@@ -58,11 +60,12 @@ const questions = [ //Adjust questions based on 'PER README' above
   ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+const writeToFile = (fileName, data) => {
 // Take in the name of the file and the data.
 
-
 // Create a .MD file, and write to it.
+fs.writeFile(`${fileName}.md`, JSON.stringify(response), (err) =>
+err ? console.error(err) : console.log('Success!'));
 
 // title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 // When license is chosen, the badge will be added near the top of the ReadMe and a notice is added to the section entitled "license"
@@ -71,12 +74,10 @@ function writeToFile(fileName, data) {
 // Table of contents - When click on links, then taken to corresponding section of readme
 }
 
-
-
 // TODO: Create a function to initialize app
 function init() {   /* See what the intro should look like from lecture video on 4-20-22.*/ 
 // This will begin the prompts
-
+writeToFile(a,b);
 }
 
 // Function call to initialize app
