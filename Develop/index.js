2d25f5fe0c3f6302e17d033472ee0d7397243b66
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-04-20 14:30:48 -0500

    added some base code for questions. Added packages 'fs' and 'inquirer'

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,36 @@
+// PER README "Everything a high quality ReadMe to have"
+// https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+
 // TODO: Include packages needed for this application
+// Input
+const inquirer = require('inquirer');
+// Export
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ // You'll need more questions.
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'username',
+    },
+    {
+      type: 'list',
+      message: 'What languages do you know?',
+      name: 'language',
+      choices: ['eng','span','french','jpn']
+    },
+    {
+      type: 'input',
+      message: 'What is your preferred method of communication?',
+      name: 'communication',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your file name',
+        name: 'fileName'
+    },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
