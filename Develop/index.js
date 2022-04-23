007e60f5abfe4e16f1717a25c5263f2d70b492d6
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-04-23 01:17:21 -0500

    cleared out old pseudocode. Added comments. Prepped for presentation.

--- Develop/index.js
+++ Develop/index.js
@@ -1,19 +1,15 @@
-// PER README "Everything a high quality ReadMe to have"
-// https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
-
-// TODO: Include packages needed for this application
+// Packages needed for this application
 // Input
 const inquirer = require('inquirer');
-const genMarkdown = require('./utils/generateMarkdown.js')
-// Export
+// Exports data
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [ //Adjust questions based on 'PER README' above
+// Array of questions/prompts for the user to input
+const questions = [
     {
         type: 'input',
-        message: 'Enter the title of your ReadMe: ',
+        message: 'Enter the title of your project: ',
         name: 'title',
     },
     {
@@ -26,66 +22,57 @@ const questions = [ //Adjust questions based on 'PER README' above
         message: 'How should your application be installed?',
         name: 'installation',
     },
-    // {
-    //     type: 'input',
-    //     message: 'How does a user utilize your application?',
-    //     name: 'usage'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Please enter your contribution guidelines:',
-    //     name: 'contribution'
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Please enter how you tested your application: ',
-    //     name: 'test'
-    // },
-     {
-         type: 'list',
-         message: 'Please select which license you would like to use: ',
-         name: 'license',
-         choices: ['Apache','GNUGPL3','MIT','BSD2','BSD3','Boost','Creative','MIT','Eclipse','GNUAffero','GNUGPL2','GNULesser','Mozilla','Unilicense']
-     }///,
-    // {
-    //     type: 'input',
-    //     message: 'Please enter your GitHub username: ',
-    //     name: 'username',
-    // },
-    // {
-    //     type: 'input',
-    //     message: 'Please enter your email address for contact purposes related to questions: ',
-    //     name: 'email',
-    // },
+    {
+        type: 'input',
+        message: 'How does a user utilize your application?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your contribution guidelines:',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'Please enter how you tested your application: ',
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: 'Please select which license you would like to use: ',
+        name: 'license',
+        choices: ['Apache','GNUGPL3','MIT','BSD2','BSD3','Boost','Creative','MIT','Eclipse','GNUAffero','GNUGPL2','GNULesser','Mozilla','Unilicense']
+    },
+    {
+        type: 'input',
+        message: 'Please enter your GitHub username: ',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email address for contact purposes related to questions: ',
+        name: 'email',
+    },
   ];
 
-// TODO: Create a function to write README file
+// Creates a new file with the data
 const writeToFile = (fileName, data) => {
-// Take in the name of the file and the data.
-// Create a .MD file, and write to it.
-fs.writeFile(`${fileName}.md`, data, (err) =>
-err ? console.error(err) : console.log('Success!'));
-
-// title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// When license is chosen, the badge will be added near the top of the ReadMe and a notice is added to the section entitled "license"
-// username and email is added to the "Questions" section of the readme
-// section with contact includes instructions on how to reach me with additional questions
-// Table of contents - When click on links, then taken to corresponding section of readme
+    fs.writeFile(`${fileName}.md`, data, (err) =>
+    err ? console.error(err) : console.log('Success!'));
 }
 
-// TODO: Create a function to initialize app
-function init() {   /* See what the intro should look like from lecture video on 4-20-22.*/ 
-let data = {};
-// This will begin the prompts
-inquirer
-    .prompt(questions)
-    .then(response => {
-        data = response;
-        // This will send the data to the file to write.
-        writeToFile('README', generateMarkdown(data));
-    });
+//  Initializes app
+function init() {
+    let data = {};
 
-
+    // This will begin the prompts for user input
+    inquirer
+        .prompt(questions)
+        .then(response => {
+            data = response;
+            // This will send the data to the file to write a README.
+            writeToFile('README', generateMarkdown(data));
+        });
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,5 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Returns a license badge based on which license is passed in
+// If there is no license, returns an empty string
 function renderLicenseBadge(license) {
   let string = '';
 
@@ -30,8 +30,8 @@ function renderLicenseBadge(license) {
   return string;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Returns the license link
+// If there is no license, returns an empty string
 function renderLicenseLink(license) {
   let string = '';
 
@@ -61,8 +61,8 @@ function renderLicenseLink(license) {
   return string;
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// Returns the license section of README
+// If there is no license, returns an empty string
 function renderLicenseSection(data) {
   let string = '';
   const time = new Date();
@@ -544,13 +544,14 @@ function renderLicenseSection(data) {
   return string;
 }
 
-// TODO: Create a function to generate markdown for README
+// Generates markdown for README
 function generateMarkdown(data) {
   // Gets badge, link, and section of license
   let badge = renderLicenseBadge(data.license);
   let link = renderLicenseLink(data.license);
   let section = renderLicenseSection(data);
-  // combines all parts of license into a single markdown
+
+  // Combines all parts of license into a single markdown
   let license = `${badge}${link}`;
 
   // Generates MarkDown
@@ -559,6 +560,7 @@ function generateMarkdown(data) {
   ## Description 
 
   ${data.description} <br>
+
   ${license}
 
   ## Table of Contents:
